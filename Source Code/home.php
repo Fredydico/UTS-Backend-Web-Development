60e1daa3eeeb49e7973d5cb2de96d1f206bc00d6
Patricyus Fredy <102717965+Fredydico@users.noreply.github.com>
2023-05-03 05:56:37 +0800

    Add files via upload

---
+++ Source Code/home.php
@@ -0,0 +1,52 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+<link href="home.css" rel="stylesheet" />
+
+<?php
+    include('navbar.php');
+?>
+
+<br><br>
+<h1 class="text-center align-self-center">
+Ujian Tengah Semester <br> Back-End Web Development
+</h1>
+<br><br>
+
+<br><br>
+<h1 class="fw-light text-center align-self-center">
+Our Teams
+</h1>
+<br><br>
+
+<div class="d-flex justify-content-evenly" id="job">
+  <div class="card shadow-lg" style="width: 30rem">
+    <img src="images/founder/adi.jpg" class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h4 class="card-title">I Putu Adi Sedana Putra</h4>
+      <h5 class="card-text fw-light">
+        Salah satu mahasiswa di ITB STIKOM Bali dengan NIM : 210040068
+      </h5>
+    </div>
+  </div>
+  <div class="card shadow-lg" style="width: 30rem">
+    <img src="images/founder/fred.jpeg" class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h4 class="card-title">Patricyus Frederyco Bara</h4>
+      <h5 class="card-text fw-light">
+        Salah satu mahasiswa di ITB STIKOM Bali dengan NIM : 210040218
+      </h5>
+    </div>
+  </div>
+  <div class="card shadow-lg" style="width: 30rem">
+    <img src="images/founder/yasa.jpg" class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h4 class="card-title">Made Bagus Adiyasa</h4>
+      <h5 class="card-text fw-light">
+        Salah satu mahasiswa di ITB STIKOM Bali dengan NIM : 210040029
+      </h5>
+    </div>
+  </div>
+</div>
+<br><br><br>
